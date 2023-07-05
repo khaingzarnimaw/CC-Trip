3b89ae93c99f9b7dee2e5760b5af8ebd09da015a
Khaing Maw <khaingmaw@Khaings-MacBook-Air.local>
2023-07-05 19:01:21 +0900

    UseEffect

---
+++ src/App.js
@@ -0,0 +1,12 @@
+
+import './App.css';
+import TripList from './components/TripList/index.js'
+
+
+function App() {
+  return (
+   <TripList/>
+  );
+}
+
+export default App;
---
+++ src/components/TripList/index.js
@@ -0,0 +1,29 @@
+import React, { useEffect, useState } from "react";
+
+export default function Index() {
+  let [trips, setTrips] = useState([]);
+
+  //useEffect
+  useEffect(() => {
+    fetch("http://localhost:3001/trips")
+      .then((res) => res.json())
+      .then((data) => {
+        setTrips(data);
+      });
+  }, []);
+  console.log(trips);
+
+  return (
+    <div>
+      <h1>Ready to go ?</h1>
+      <ul>
+        {trips.map((trip) => (
+          <li key={trip.id}>
+            <h3>{trip.name}</h3>
+            <p>price - {trip.price}</p>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
