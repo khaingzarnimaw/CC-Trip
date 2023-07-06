9642b9eb9cb0bb3515a33106928bcc04e1e13c16
Khaing Maw <khaingmaw@Khaings-MacBook-Air.local>
2023-07-06 16:01:51 +0900

    useEffect hook

--- src/components/TripList/index.js
+++ src/components/TripList/index.js
@@ -1,21 +1,29 @@
 import React, { useEffect, useState } from "react";
+import './index.css'
 
 export default function Index() {
   let [trips, setTrips] = useState([]);
 
+  let [url,setUrl]= useState('"http://localhost:3001/trips"')
+
   //useEffect
   useEffect(() => {
-    fetch("http://localhost:3001/trips")
+    //၂button ကိုနှိပ်လိုက်မယ်
+    //url က update ဖစ်သွားတယ်/useeffect ကတခါrunပါတယ်
+    //http:နဲ့ string လေးက၀င်လာတယ်/myanmar data ကိုပြတယ်/setTripsကိုသုံးပီး tripsတွေကိ update လုပ်တယ်/componentက rerender ကျတယ်/return အောက်ကdiv က ပြန်အလုပ်လုပ်တယ်
+    fetch(url)
       .then((res) => res.json())
       .then((data) => {
         setTrips(data);
       });
-  }, []);
+  }, [url]);//[]ထဲမှာ
   console.log(trips);
 
   return (
     <div>
       <h1>Ready to go ?</h1>
+      <button onClick={()=>setUrl('http://localhost:3001/trips')} className="A">All</button>
+      <button  onClick={()=>setUrl('http://localhost:3001/trips?location=Myanmar')} className="B">Trip in Myanmar</button>
       <ul>
         {trips.map((trip) => (
           <li key={trip.id}>
