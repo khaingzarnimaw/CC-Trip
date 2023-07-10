959964c544775971f2d7bf2c59a3daac3c5fcad1
Khaing Maw <khaingmaw@Khaings-MacBook-Air.local>
2023-07-11 03:57:14 +0900

    loading & Error control

--- src/components/TripList/index.js
+++ src/components/TripList/index.js
@@ -4,21 +4,27 @@ import './index.css'
 
 export default function Index() {
 
-  let [url,setUrl]= useState('"http://localhost:3001/trips"')
+  let [url,setUrl]= useState('"http://localhost:3001/trips"');
 
   let { data: trips ,loading , error } = useFetch(url)
 
+  console.log(trips)
+
   return (
     <div className="container">
-      {error && <p> {error} </p>}
-     { !!error && <div className ="flex-container">
+       {/* error ကိုပြချင်ရင်  */}
+      {/* {error && <p> {error} </p>} */} 
+     {/* { !error && <div className ="flex-container"> */}
+     <div className ="flex-container">
      <h1>Ready to go ?</h1>
-    { loading &&  <p>Loading</p> }
+    { loading &&  <p>Loading trips</p> }
       <div>
       <button onClick={()=>setUrl('http://localhost:3001/trips')} >All</button>
       <button  onClick={()=>setUrl('http://localhost:3001/trips?location=Myanmar')} >Trip in Myanmar</button>
       </div>
       <ul className='trips-list'>
+
+        {/* condition စစ်ထားတယ် null ပေါ်နေလို့ */}
         {trips && trips.map((trip) => (
           <li key={trip.id} className='trip'>
             <h3>{trip.name}</h3>
@@ -26,7 +32,7 @@ export default function Index() {
           </li>
         ))}
       </ul>
-     </div>}
+     </div>
     </div>
   );
 }
--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -3,7 +3,7 @@ import { useEffect,useState } from "react";
 function useFetch(url){
 
    let [data,setData]= useState(null);
-   let [loading,setLoading]= useState(false)
+   let [loading,setLoading]= useState(false)//loading state 
    let [error,setError] = useState(null);
 //useEffect (fetch -> first render ->useEffect)
   //fetch -> dyanmic -> url
@@ -12,15 +12,15 @@ function useFetch(url){
     setLoading(true);
     fetch(url)
     .then((res) => {
-        // console.log(res);
+        // console.log(res);//error ကိုစစ်ပါ
         if(!res.ok){
-         throw Error ('something went wrong')
+         throw Error ('something went wrong');//လှမ်းပစ်လိုက်တဲ့ error ကို catch နဲ့ ပြန်ဖမ်းလို့ရ
         }
         return res.json();
     })
     .then((data) => {
       setData(data);
-      setError(null);
+      setError(null);//data မှန်သွားခဲ့ရင် error မတတ်တော့ပါဘူး so write
       setLoading(false);
     })
     .catch(e => {
